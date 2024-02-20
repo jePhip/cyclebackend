6394c074ba0dfd7e131ff6d8f15c910635dc5c4f
JePhip <jesaiahphipps@gmail.com>
2024-02-20 14:59:47 -0600

    merging

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ export const lucia = new Lucia(adapter, {
 const app = new Elysia() //
   .use(
     cors({
-      credentials: true,
+      origin: "http://localhost:8080",
     })
   ) //
   .use(

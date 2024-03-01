8514d46295782c6c0dbe1772c3b37b181ab3cc48
JePhip <jesaiahphipps@gmail.com>
2024-02-29 18:51:51 -0600

    rebuild

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ const app = new Elysia() //
     cors({
       credentials: true,
       origin: [
-        "https://bolivarcyclingroutes.com",
+        "https://www.bolivarcyclingroutes.com/"
       ],
     })
   ) //
